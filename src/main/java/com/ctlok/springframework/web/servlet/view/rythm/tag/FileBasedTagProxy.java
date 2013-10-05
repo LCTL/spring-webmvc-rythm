683e251ef1e72af0bfe8086c6f4b4fc875a35d66
Lawrence Cheung <lawrence0819@gmail.com>
2013-10-05 19:08:57 +0800

    File based tag support @renderBody()

--- src/main/java/com/ctlok/springframework/web/servlet/view/rythm/tag/FileBasedTagProxy.java
+++ src/main/java/com/ctlok/springframework/web/servlet/view/rythm/tag/FileBasedTagProxy.java
@@ -2,6 +2,7 @@ package com.ctlok.springframework.web.servlet.view.rythm.tag;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.List;
 import org.rythmengine.Rythm;
 import org.rythmengine.template.ITemplate;
 import org.rythmengine.template.JavaTagBase;
+import org.rythmengine.template.TagBase;
+import org.springframework.beans.BeanUtils;
 import org.springframework.core.io.Resource;
 
 import com.ctlok.springframework.web.servlet.view.rythm.Helper;
@@ -22,6 +25,7 @@ public class FileBasedTagProxy extends JavaTagBase {
     
     private final File file;
     private final String tagName;
+    private final Method templateSetBodyMethod;
     
     public FileBasedTagProxy(final FileBasedTag fileBasedTag) throws IOException{
         
@@ -29,6 +33,7 @@ public class FileBasedTagProxy extends JavaTagBase {
         this.tagName = fileBasedTag.getTagName() == null ? 
                 getDefaultTagName(fileBasedTag.getResource()) : fileBasedTag.getTagName();
                 
+        this.templateSetBodyMethod = findTagBaseSetBodyMethod();
     }
     
     private String getDefaultTagName(final Resource resource){
@@ -37,6 +42,18 @@ public class FileBasedTagProxy extends JavaTagBase {
         final int indexOfExt = templateFileName.lastIndexOf(".");
         
         return indexOfExt == -1 ? templateFileName : templateFileName.substring(0, indexOfExt);
+        
+    }
+    
+    private Method findTagBaseSetBodyMethod(){
+        
+        final Method method = 
+                BeanUtils.findDeclaredMethod(TagBase.class, "setBody", 
+                        new Class<?>[]{org.rythmengine.template.ITag.__Body.class});
+        
+        method.setAccessible(true);
+        
+        return method;
         
     }
     
@@ -71,6 +88,16 @@ public class FileBasedTagProxy extends JavaTagBase {
             template = Rythm.engine().getTemplate(file, params.asMap());
         
         }
+
+        try {
+            
+            templateSetBodyMethod.invoke(template, body);
+            
+        } catch (Exception e) {
+            
+            throw new IllegalStateException(e);
+            
+        } 
         
         this.p(template.render());
         
